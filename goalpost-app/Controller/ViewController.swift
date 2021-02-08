42063557334d07b68650ad486a5dab752c59e735
Artur Zarzecki <zarzecki.art@icloud.com>
2021-02-08 14:31:03 +0100

    Set up project

---
+++ goalpost-app/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Artur Zarzecki on 05/02/2021.
+//  Copyright © 2021 Artur Zarzecki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
