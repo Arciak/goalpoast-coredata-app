37c5aa49b05b74034d80052be962d34489731df5
Artur Zarzecki <zarzecki.art@icloud.com>
2021-02-10 10:04:19 +0100

    Build goal cell. Add fucntion to init text in cell.

---
+++ goalpost-app/Views/GoalCell.swift
@@ -0,0 +1,23 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Artur Zarzecki on 09/02/2021.
+//  Copyright © 2021 Artur Zarzecki. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+    
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(goalProgressAmount)
+    }
+
+}
