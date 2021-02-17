b3de829cbf41751ceb8042cba2ca3e50c01a5e1d
Artur Zarzecki <zarzecki.art@icloud.com>
2021-02-17 16:55:26 +0100

    Dismiss from createGoalVC and Add finishVC on GoalsVC

--- goalpost-app/Extensions/UIViewControllerExt.swift
+++ goalpost-app/Extensions/UIViewControllerExt.swift
@@ -20,6 +20,18 @@ extension UIViewController {
         present(viewControllerToPresent, animated: false, completion: nil) // we have overwritten the animation, if we call true it will call a default animation
     }
     
+    func presentSecondaryDetail(_ viewControllerToPresent: UIViewController) {
+        let transition = CATransition()
+        transition.duration = 0.3
+        transition.type = .push
+        
+        guard let presentedViewController = presentedViewController else { return }
+        presentedViewController.dismiss(animated: false) {
+            self.view.window?.layer.add(transition, forKey: kCATransition)
+            self.present(viewControllerToPresent, animated: false, completion: nil)
+        }
+    }
+    
     func dismissDetail() {
         let transition = CATransition()
         transition.duration = 0.3
